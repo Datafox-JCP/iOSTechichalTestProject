d77c09d416a376f43aa87e61db1241081e63758a
Datafox-JCP <datafox@outlook.com>
2022-07-27 21:18:28 -0500

    Initial Commit

---
+++ iOSTechichalTestProject/iOSTechichalTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSTechichalTestProjectApp.swift
+//  iOSTechichalTestProject
+//
+//  Created by Juan Hernandez Pazos on 27/07/22.
+//
+
+import SwiftUI
+
+@main
+struct iOSTechichalTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
