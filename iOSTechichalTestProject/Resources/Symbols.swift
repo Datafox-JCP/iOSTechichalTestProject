1377e4da614feed410f76cae14dd62683db263bc
Datafox-JCP <datafox@outlook.com>
2022-07-27 21:36:03 -0500

    Add rSFSymbols and Colors as enum resources

---
+++ iOSTechichalTestProject/Resources/Symbols.swift
@@ -0,0 +1,14 @@
+//
+//  Symbols.swift
+//  iOSTechichalTestProject
+//
+//  Created by Juan Carlos Pazos on 27/07/22.
+//
+
+import SwiftUI
+
+enum Symbols {
+    static let person = Image(systemName: "person.2")
+    static let gear = Image(systemName: "gear")
+    static let plus = Image(systemName: "plus")
+}
